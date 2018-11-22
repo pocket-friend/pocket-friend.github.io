0ec5bdf47f4a0e2b157ba84ece6a9bf4169f97d9
pocket-friend <45273060+pocket-friend@users.noreply.github.com>
2018-11-22 22:56:24 +0200

    Add files via upload

---
+++ precache-manifest.851b32bb0902e680915f8ac8487576ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3cc3c8be50d74fdb8a9a",
+    "url": "/static/js/main.3cc3c8be.chunk.js"
+  },
+  {
+    "revision": "da338d2a22aedffb5394",
+    "url": "/static/js/1.da338d2a.chunk.js"
+  },
+  {
+    "revision": "3cc3c8be50d74fdb8a9a",
+    "url": "/static/css/main.cd861248.chunk.css"
+  },
+  {
+    "revision": "da338d2a22aedffb5394",
+    "url": "/static/css/1.313e241d.chunk.css"
+  },
+  {
+    "revision": "c3cad992a5eead52472f0e8c19989e47",
+    "url": "/index.html"
+  }
+];
